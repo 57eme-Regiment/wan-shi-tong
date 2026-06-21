11e44b0754be9ef278d3d65894c457f6e4cc7558
Dercraker <antoine.capitain@gmail.com>
2026-06-21 21:38:41 +0200

    Add missing export

--- packages/auth-package/src/browser/index.ts
+++ packages/auth-package/src/browser/index.ts
@@ -1,7 +1,7 @@
-export { configureAuth } from './config';
-export { createAuthClient } from './auth-client';
 export { createAccessClient } from './access-client';
 export type { AccessClient } from './access-client';
-export { useAccess, useHasPermission } from './hooks';
 export { createAdminClient } from './admin-client';
 export type { AdminClient } from './admin-client';
+export { createAuthClient } from './auth-client';
+export { configureAuth, getConfig } from './config';
+export { useAccess, useHasPermission } from './hooks';
